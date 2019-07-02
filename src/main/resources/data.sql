00faa4c71df8aa83dda0877e66aa39f7c2a79de4
P2883280 <C-MequannintMunye.Zeru@charter.com>
2019-07-02 16:15:16 -0500

    data inserted to DB

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,8 @@
 insert into student values(11,'Ahmed', 'EP123456');
 insert into student values(12,'Bereket', 'EP654321');
+insert into student values(13,'Moke', 'EP456789');
+insert into student values(14,'Abebe', 'EP654322');
+insert into student values(15,'Teklay', 'EP123453');
+insert into student values(16,'Abdisa', 'EP654324');
+insert into student values(17,'Tolosa', 'EP123455');
+insert into student values(18,'Tigist', 'EP654326');
